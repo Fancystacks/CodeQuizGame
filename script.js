9c6147b8dfe8b2efbc7c09d20e031dcb28cfd4a0
Leah Migacz <lmigacz@msudenver.edu>
2019-12-17 10:01:23 -0700

    added question information

--- script.js
+++ script.js
@@ -15,19 +15,19 @@ var questions = [
       answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "Arrays can be naviagted using ____.",
+      choices: ["dot notation", "hyphen notation", "parentheses", "functions"],
+      answer: "dot notation"
     },
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        title: "A shortened version of JavaScript:",
+        choices: ["vanilla", "jquery", "node", "backticks"],
+        answer: "jquery"
       },
       {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "The local storage can only hold ____.",
+        choices: ["true values", "arrays", "strings", "numbers"],
+        answer: "strings"
       },
   ];
     
